781f96964ced9b5e526a543cfb1c1e52f780d8c0
ABKodes <abiyshiferaw12@gmail.com>
2023-11-08 16:03:22 +0300

    added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
   return (
     <html>
       <body>
         <Header />
         <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <Footer/>
       </body>
     </html>
   );
