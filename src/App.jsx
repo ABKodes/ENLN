9c7e2eb49634a33901654e5c66c434585c68ad92
ABKodes <abiyshiferaw12@gmail.com>
2023-11-08 15:48:55 +0300

    configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 function App() {
   return (
-    <html data-theme = "light">
+    <html>
       <body>
         <Header />
         <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
