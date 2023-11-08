651aba24dfe0ccea2aabaee04eda9e721fbdeb94
ABKodes <abiyshiferaw12@gmail.com>
2023-11-08 13:38:43 +0300

    configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
   daisyui: {
-    themes: [],
+    themes: ["light"],
   },
+  plugins: [require("daisyui")],
 };
