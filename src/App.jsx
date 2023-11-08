f9ff819353d04f8da1fa8a54d635183f3d220025
ABKodes <abiyshiferaw12@gmail.com>
2023-11-08 13:39:22 +0300

    added header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import Header from "./components/Header";
 function App() {
-  return <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+  return (
+    <html data-theme = "light">
+      <body>
+        <Header />
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+        <h1 className="m-5 p-4 text-3xl underline">This is the start</h1>;
+      </body>
+    </html>
+  );
 }
 
 export default App;
