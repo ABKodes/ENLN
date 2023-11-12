14186f84bf2eaa2046f30de930ad44e05d14ba7e
ABKodes <abiyshiferaw12@gmail.com>
2023-11-12 18:15:18 +0300

    update the hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,24 +5,40 @@ import '../Fonts/fonts.css';
 
 const Hero = () => {
   return (
-    <div className='text-white relative'>
-      <div className='relative'>
-        <img className='w-full h-screen object-cover scale-x-[-1]' src={background} alt="" />
-        <div className='absolute top-0 left-0 w-full h-full'>
-          <div className='w-full h-full bg-gradient-to-b from-black opacity-70'></div>
+    <>
+      {/* Hero section */}
+      <div
+        className="hero min-h-screen "
+        style={{
+          backgroundImage: `url(${background})`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-40"></div>
+        <div
+          className="hero-content text-center text-neutral-content"
+          style={{ fontFamily: "Playfair Display" }}
+        >
+          <div className="min-w-md">
+            <h1 className="p-2 text-9xl font-bold tracking-widest text-primary">
+              ENLN
+            </h1>
+            <p className=" space-x-2 text-6xl font-bold text-white my-2">
+              Ethiopia Nutrition Leaders Network
+            </p>
+            <p className="mb-5 font-sans text-white ">
+              At ENLN, we are committed to fostering nutrition leadership that
+              transforms communities and builds a healthier nation. Our 
+              collaborative network brings together experts, advocates, and
+              change makers dedicated to addressing nutrition challenges in
+              Ethiopia.
+            </p>
+            <button className="mx-auto my-6 w-[200px] rounded-md bg-primary py-3 font-sans font-medium text-white">
+              Join Us
+            </button>
+          </div>
         </div>
       </div>
-      <div style={{ fontFamily: "Playfair Display" }} className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center'>
-        <p className='text-primary font-bold p-2 text-9xl'>
-          E N L N
-        </p>
-        <h1 className=' md:text-6xl sm:text-6xl text-4xl font-bold md:py-6 text-white '>
-          Ethiopia Nutrition Leaders Network
-        </h1>
-        <p className='md:text-2xl text-xl font-bold text-white'>Become a member</p>
-        <button className='bg-primary font-sans w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white'>Join Us</button>
-      </div>
-    </div>
+    </>
   );
 };
 
