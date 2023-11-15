14776d717de0f68961434554a95a144f08235e83
ABKodes <abiyshiferaw12@gmail.com>
2023-11-15 21:56:16 +0300

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,11 +20,12 @@ const Hero = () => {
             <h1 className="p-2 text-9xl font-bold tracking-widest text-primary">
               ENLN
             </h1>
-            <p className=" space-x-2 text-6xl font-bold text-white my-2">
+            <p className=" my-2 space-x-2 text-6xl font-bold text-white">
               Ethiopia Nutrition Leaders Network
             </p>
             <p className="mb-5 font-sans text-white ">
-              Create Visionary Leaders for the Journey to End Malnutrition in Ethiopia.
+              Create Visionary Leaders for the Journey to End Malnutrition in
+              Ethiopia.
             </p>
             <button className="mx-auto my-6 w-[200px] rounded-md bg-primary py-3 font-sans font-medium text-white">
               Join Us
@@ -32,6 +33,40 @@ const Hero = () => {
           </div>
         </div>
       </div>
+
+      {/* Coming Soon */}
+      <div className="mb-5 h-screen w-full bg-[#efefef]">
+        <div className=" h-3/12 mx-auto mt-5 w-6/12 bg-white p-8">
+          <h1 className="p-5 text-center">Coming Soon</h1>
+          <p className="p-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div className="card  font-sans lg:card-side my-4 w-11/12 mx-auto">
+          <div className="card-body flex items-center justify-center p-6">
+            <h2 className="card-title">New album is released!</h2>
+            <p className=''>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ligula
+              ullamcorper malesuada proin libero. Vulputate ut pharetra sit amet
+              aliquam id diam maecenas. Aliquet nec ullamcorper sit amet risus
+              nullam. Hac habitasse platea dictumst vestibulum rhoncus. Pretium
+              lectus quam id leo in vitae turpis. Nisi quis eleifend quam
+              adipiscing vitae proin sagittis. Lacus vel facilisis volutpat est.
+            </p>
+          </div>
+          <div className="max-w-[600px]">
+            <img src={background} className="mx-auto w-full" />
+          </div>
+        </div>
+      </div>
+        {/* What we stand for */}
     </>
   );
 };
