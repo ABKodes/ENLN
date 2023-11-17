6395fa48c97aad94d00879bcc56445b97791f2aa
ABKodes <abiyshiferaw12@gmail.com>
2023-11-17 10:54:46 +0300

    app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home"
-import About from "./pages/About"
-import NewsDetail from "./pages/NewsDetail";
-import Register from "./pages/Register";
-import Blog from "./pages/Blog"
-import News from "./pages/News";
-import Programs from "./pages/Programs";
-import Resources from "./pages/Resources";
-import Contacts from "./pages/Contacts";
-import Events from "./pages/Events";
-import Membership from "./pages/Membership";
-import AnnalForum from "./pages/AnnualForum"
 function App() {
   return (
     <html>
       <body>
         <Header />
         <Home />
-        <About />
-        <News/>
-        <Blog />
-        <Programs/>
-        <Resources/>
-        <Contacts/>
-        <Register />
-        <Events/>
-        <Membership />
-        <NewsDetail/>
-        <AnnalForum />
         <Footer />
       </body>
     </html>
