f0f985bb39e82fa0e26e1a89dc4fcef0de85c10f
ABKodes <abiyshiferaw12@gmail.com>
2023-11-17 10:58:10 +0300

    header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,98 @@
-import Logo from '../assets/logo.png'
-import { Link } from 'react-router-dom';
+import Logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+
 function Header() {
+  const routes = [
+    {
+      path: "/",
+      label: "Home",
+    },
+    {
+      path: "/about",
+      label: "About",
+    },
+    {
+      path: "/program",
+      label: "Program",
+    },
+    {
+      path: "/news-and-events",
+      label: "News and Events",
+      subRoutes: [
+        {
+          path: "/news",
+          label: "News",
+        },
+        {
+          path: "/events",
+          label: "Events",
+        },
+        {
+          path: "/annual-forum",
+          label: "Annual Forum",
+        },
+      ],
+    },
+    {
+      path: "/blog",
+      label: "Blog",
+    },
+    {
+      path: "/resources",
+      label: "Resources",
+      subRoutes: [
+        {
+          path: "/publication",
+          label: "Publication",
+        },
+        {
+          path: "/training-material",
+          label: "Training Material",
+        },
+        {
+          path: "/leadership-material",
+          label: "Leadership Material",
+        },
+      ],
+    },
+  ];
+
   return (
-    <div className="sticky top-0 z-10 bg-[#e8e8e8] z-20">
+    <div className="sticky top-0 z-10 z-20 bg-[#e8e8e8]">
       <div className="navbar mx-auto w-10/12">
         <div className="navbar-start">
-          <img src={Logo} alt="logo" className="w-[23%] h-[23%]" />
+          <img src={Logo} alt="logo" className="h-[23%] w-[23%]" />
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li>
-              <Link>About</Link>
-            </li>
-            <li>
-              <a>Program</a>
-            </li>
-            <li tabIndex={0}>
-              <details>
-                <summary>News and Events</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>News</a>
-                  </li>
-                  <li>
-                    <a>Events</a>
-                  </li>
-                  <li>
-                    <a>Annual Forum</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Blog</a>
-            </li>
-            <li tabIndex={0}>
-              <details>
-                <summary>Resources</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Publication</a>
-                  </li>
-                  <li>
-                    <a>Training Material</a>
-                  </li>
-                  <li>
-                    <a>Leadership Material</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
+            {routes.map((item, index) => (
+              <li key={index}>
+                {item.subRoutes ? (
+                  <details>
+                    <summary>
+                      <Link to={item.path}>{item.label}</Link>
+                    </summary>
+                    <ul className="p-2">
+                      {item.subRoutes.map((subItem, subIndex) => (
+                        <li key={subIndex}>
+                          <Link to={subItem.path}>{subItem.label}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </details>
+                ) : (
+                  <Link to={item.path}>{item.label}</Link>
+                )}
+              </li>
+            ))}
           </ul>
         </div>
         <div className="navbar-end">
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <Link to="#" className="btn btn-primary btn-md text-center">
+            Join Us
+          </Link>
+          <div className="dropdown dropdown-end lg:hidden">
+            <label tabIndex={0} className="btn btn-ghost">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -74,47 +112,27 @@ function Header() {
               tabIndex={0}
               className="menu dropdown-content rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-2 shadow"
             >
-              <li>
-                <a>About</a>
-              </li>
-              <li>
-                <a>Program</a>
-              </li>
-              <li>
-                <a>News and Events</a>
-                <ul className="p-2">
-                  <li>
-                    <a>News</a>
-                  </li>
-                  <li>
-                    <a>Events</a>
-                  </li>
-                  <li>
-                    <a>Annual Forum</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Blog</a>
-              </li>
-              <li>
-                <a>Resources</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Publication</a>
-                  </li>
-                  <li>
-                    <a>Training Material</a>
-                  </li>
-                  <li>
-                    <a>Leadership Material</a>
-                  </li>
-                </ul>
-              </li>
+              {routes.map((item, index) => (
+                <li key={index}>
+                  {item.subRoutes ? (
+                    <details>
+                      <summary>
+                        <Link to={item.path}>{item.label}</Link>
+                      </summary>
+                      <ul className="p-2">
+                        {item.subRoutes.map((subItem, subIndex) => (
+                          <li key={subIndex}>
+                            <Link to={subItem.path}>{subItem.label}</Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </details>
+                  ) : (
+                    <Link to={item.path}>{item.label}</Link>
+                  )}
+                </li>
+              ))}
             </ul>
-            <a className="btn btn-md btn-primary">Join Us</a>
-          <div className="hidden lg:block">
-          </div>
           </div>
         </div>
       </div>
